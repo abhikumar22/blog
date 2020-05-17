4ee2af70d2530a8061b756efd68ab4b9fd699f29
abhikumar22 <abhi.ckp1002@gmail.com>
2020-05-17 16:58:04 +0530

    removing footer

--- src/pages/App.js
+++ src/pages/App.js
@@ -179,10 +179,10 @@ function App() {
             </nav> */}
           </div>
         </div>
-        <footer className="footer text-center py-2 theme-bg-dark">
+        {/* <footer className="footer text-center py-2 theme-bg-dark">
           <small className="copyright">Designed with <i className="fas fa-heart" style={{ backgroundColor: "#fb866a" }}></i> by <a
             href="http://themes.3rdwavemedia.com">Xiaoying Riley</a> for developers</small>
-        </footer>
+        </footer> */}
       </div>
     </div>
   );
