0408e3981cabae27c61a905dbedbb9fd9415b8cf
abhikumar22 <abhi.ckp1002@gmail.com>
2020-05-21 00:57:32 +0530

    added not found page

---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { URLS, APIS } from '../utils/constants'
+
+import '../css/App.css'
+
+
+export default class NotFoundPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            blogArray: []
+        };
+        // this.clickToPush = this.clickToPush.bind(this);
+    }
+
+    componentDidMount() {
+        fetch(URLS.HEROKU + APIS.GET_ALL_BLOG, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+
+            },
+            body: JSON.stringify({
+                username: "11",
+            })
+        })
+            .then(response => response.json())
+            .then(res => {
+                // console.log(json)
+                this.setState({ blogArray: res }, () => {
+                    console.log("ll", this.state.blogArray)
+                })
+                // history.push('/AllUserScreen', { uid: res.uid })
+
+            })
+            .catch(error => console.log('Authorization failed : ' + error.message));
+    }
+
+    // clickToPush=(data)=> {
+    //   // console.log("pppp",data)
+    //   history.push('/BlogDetail', { blogDetailValues:data })
+    // }
+
+
+    render() {
+        return (
+            <div className="App imgt w-100 h-100 col-12" >
+                
+            </div>
+        );
+    }
+}
+
--- src/routes/router.js
+++ src/routes/router.js
@@ -3,6 +3,8 @@ import { Router, Switch, Route } from "react-router-dom";
 import Home from "../pages/App";
 import BlogDetail from "../pages/BlogDetail";
 import AddPost from "../pages/AddPost";
+import NotFoundPage from "../pages/NotFoundPage";
+
 import history from './history';
 
 export default class Routes extends Component {
@@ -13,6 +15,7 @@ export default class Routes extends Component {
                     <Route path="/" exact component={Home} />
                     <Route path="/addpost" component={AddPost} />
                     <Route path="/blogDetail" component={BlogDetail} />
+                    <Route path='*' exact={true} component={NotFoundPage} />
                 </Switch>
             </Router>
         )
