6ceb47bc413ae965a83ab3467aa47c487947df08
abhikumar22 <abhi.ckp1002@gmail.com>
2020-05-22 01:47:21 +0530

    added footer changes

--- src/pages/BlogDetail.js
+++ src/pages/BlogDetail.js
@@ -6,6 +6,8 @@ import { processedDate } from '../utils/HelperFunctions';
 
 import SocialComponent from '../components/SocialComponent'
 import { STRINGS, SOCIAL_URL, SOCIAL_ICONS, URLS, APIS } from '../utils/constants'
+import history from '../routes/history';
+
 
 
 
@@ -23,54 +25,68 @@ export default class BlogDetail extends React.Component {
             <div style={{ width: '100%', height: '100%' }} className="App">
                 <div className="col-10 col-sm-10 col-md-6 col-lg-6 col-xl-6" style={{ margin: 'auto', marginTop: 30 }}>
                     <div>
-                        <h4><strong>abhikumar.xyz</strong></h4>
-                        <div style={{ textAlign: 'justify' }} className="mt-2">
-                            <p style={{ fontSize: 14 }}>Tech | Motivation | Part Time Chef</p>
+                        <h4 onClick={() => {
+                            history.push('/')
+
+                        }} className="cursorPointer" style={{ margin: 0, padding: 0, textAlign: 'justify' }}><strong>abhikumar.xyz</strong></h4>
+                        <div style={{ textAlign: 'justify' }} className="">
+                            <p style={{ margin: 0, padding: 0, fontSize: 14 }}>Tech | Motivation | Part Time Chef</p>
                         </div>
                     </div>
-                    <hr style={{ marginTop: 30 }} />
-                    <div style={{ marginTop: 30 }}>
-                        <h2>{this.props.location.state.blogDetailValues.title}</h2>
+                    <hr style={{ marginTop: 20 }} />
+                    <div style={{ marginTop: 20, margin: 0, padding: 0, }}>
+                        <h2 style={{ margin: 0, padding: 0, }}>{this.props.location.state.blogDetailValues.title}</h2>
                         <div>
-                            <p style={{ marginTop: 0, textAlign: 'justify' }} className="text-muted mt-1">Published {processedDate(this.props.location.state.blogDetailValues.created_at)} • {this.props.location.state.blogDetailValues.read_interval_in_minutes} min read</p>
+                            <p style={{ margin: 0, padding: 0, textAlign: 'justify' }} className="text-muted">Published {processedDate(this.props.location.state.blogDetailValues.created_at)} • {this.props.location.state.blogDetailValues.read_interval_in_minutes} min read</p>
                         </div>
-                        <p style={{ marginTop: 0, textAlign: 'justify' }}>{parse(this.props.location.state.blogDetailValues.content_of_blog)}</p>
+                        <p style={{ marginTop: 20, textAlign: 'justify' }}>{parse(this.props.location.state.blogDetailValues.content_of_blog)}</p>
 
                     </div>
                     <hr />
                     <div
-                        className="text-center"
+                        // className="text-center"
                     >
-                        <ul className="social-list list-inline py-3 mx-auto">
+                        <ul className="social-list list-inline py-3 mx-auto row">
                             {/* <li className="list-inline-item"> */}
-                            <footer>
-                                <small>&copy; Copyright 2020 Abhishek Kumar</small>
-                            </footer>
-                            {/* </li> */}
-                            <SocialComponent
-                                link={SOCIAL_URL.LINKEDIN}
-                                icons={SOCIAL_ICONS.LINKEDIN}
-                            />
-                            <SocialComponent
-                                link={SOCIAL_URL.GITHUB}
-                                icons={SOCIAL_ICONS.GITHUB}
-                            />
-                            <SocialComponent
-                                link={SOCIAL_URL.STACKOVERFLOW}
-                                icons={SOCIAL_ICONS.STACKOVERFLOW}
-                            />
-                            <SocialComponent
-                                link={SOCIAL_URL.FACEBOOK}
-                                icons={SOCIAL_ICONS.FACEBOOK}
-                            />
-                            <SocialComponent
-                                icons={SOCIAL_ICONS.INSTAGRAM}
-                                link={SOCIAL_URL.INSTAGRAM}
-                            />
-                            <SocialComponent
-                                icons={SOCIAL_ICONS.TWITTER}
-                                link={SOCIAL_URL.TWITTER}
-                            />
+                            <div style={{ width: 50, height: 50, borderRadius: 30 }}>
+                                <img className="profile-image mb-3 rounded-circle mx-auto"
+                                    src={require("../images/pp.jpeg")}
+                                    alt={"Pics"}
+                                    style={{ width: 50, height: 50, borderRadius: 30 }}
+                                />
+                            </div>
+                            <div className="ml-3">
+                                <footer>
+                                    <small>&copy; Copyright 2020 Abhishek Kumar</small>
+                                </footer>
+                                {/* </li> */}
+                                <SocialComponent
+                                    link={SOCIAL_URL.LINKEDIN}
+                                    icons={SOCIAL_ICONS.LINKEDIN}
+                                />
+                                <SocialComponent
+                                    link={SOCIAL_URL.GITHUB}
+                                    icons={SOCIAL_ICONS.GITHUB}
+                                />
+                                <SocialComponent
+                                    link={SOCIAL_URL.STACKOVERFLOW}
+                                    icons={SOCIAL_ICONS.STACKOVERFLOW}
+                                />
+                                <SocialComponent
+                                    link={SOCIAL_URL.FACEBOOK}
+                                    icons={SOCIAL_ICONS.FACEBOOK}
+                                />
+                                <SocialComponent
+                                    icons={SOCIAL_ICONS.INSTAGRAM}
+                                    link={SOCIAL_URL.INSTAGRAM}
+                                />
+                                <SocialComponent
+                                    icons={SOCIAL_ICONS.TWITTER}
+                                    link={SOCIAL_URL.TWITTER}
+                                />
+
+                            </div>
+
                         </ul>
                     </div>
                 </div>
